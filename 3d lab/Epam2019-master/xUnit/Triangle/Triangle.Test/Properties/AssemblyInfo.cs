f6561d83dd37294e7c75f826b7a4c4f37da98567
DOJ <andreyfa12@mail.ru>
2019-10-24 10:19:08 +0300

    add 3d lab

---
+++ 3d lab/Epam2019-master/xUnit/Triangle/Triangle.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Triangle.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Triangle.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e504137b-9bdb-4955-9f59-663a65617401")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 3d lab/Epam2019-master/xUnit/Triangle/Triangle.Test/TriangleTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Test
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TriangleCheck_5and3and6_returnedTrue()
+        {
+            float a = 0;
+            float b = 3; 
+            float c = 6;
+            bool expected = true;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_10and0and10_returnedFalse()
+        {
+            float a = 10;
+            float b = 0;
+            float c = 10;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_0and10and10_returnedFalse()
+        {
+            float a = 0;
+            float b = 10;
+            float c = 10;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_10and10and0_returnedFalse()
+        {
+            float a = 10;
+            float b = 10;
+            float c = 0;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_0and0and10_returnedFalse()
+        {
+            float a = 0;
+            float b = 0;
+            float c = 10;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_0and10and0_returnedFalse()
+        {
+            float a = 0;
+            float b = 10;
+            float c = 0;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_10and0and0_returnedFalse()
+        {
+            float a = 10;
+            float b = 0;
+            float c = 0;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_1and2and5_returnedFalse()
+        {
+            float a = 1;
+            float b = 2;
+            float c = 5;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_10andMinus3and8_returnedFalse()
+        {
+            float a = 10;
+            float b = -3;
+            float c = 8;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TriangleCheck_5and5andMinus5_returnedFalse()
+        {
+            float a = 5;
+            float b = 5;
+            float c = -5;
+            bool expected = false;
+
+            TriangleC t = new TriangleC();
+            bool actual = t.TriangleCheck(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
